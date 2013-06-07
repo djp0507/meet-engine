620ae900af95c4ff687e7c6b9cfc1266c64fdad8
rogershen <rogershen@653f6834-aab9-c84e-bf21-5865e8c89758>
2013-06-07 02:18:55 +0000

    review:50654
    完善播放接口说明
    
    git-svn-id: http://svn01/repositories/r02/MediaPipeline/meet/engine@88931 653f6834-aab9-c84e-bf21-5865e8c89758

--- player/player.h
+++ player/player.h
@@ -1,6 +1,7 @@
 #ifndef IPLAYER_H
 #define IPLAYER_H
 
+//视频媒体信息
 typedef struct MediaInfo {
 	int32_t duration_ms; //in millisecond 
 	long long size_byte; //in byte
@@ -33,7 +34,7 @@ typedef struct MediaInfo {
     
 } MediaInfo;
 
-
+//播放器回调状态分类
 enum media_event_type {
     MEDIA_NOP               = 0, // interface test message
     MEDIA_PREPARED          = 1,
@@ -46,44 +47,25 @@ enum media_event_type {
     MEDIA_COMPATIBILITY_TEST_COMPLETE              = 300,
 };
 
-enum media_error_type {
-    // 0xx
-    MEDIA_ERROR_UNKNOWN = 1,
-    // 1xx
-    MEDIA_ERROR_SERVER_DIED = 100,
-    // 2xx
-    MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK = 200,
-    // 3xx
-};
-
-
+//播放器回调信息详细分类
 enum media_info_type {
-    // 0xx
     MEDIA_INFO_UNKNOWN = 1,
-    // 7xx
-    // The video is too complex for the decoder: it can't decode frames fast
-    // enough. Possibly only the audio plays fine at this stage.
     MEDIA_INFO_VIDEO_TRACK_LAGGING = 700,
-    // MediaPlayer is temporarily pausing playback internally in order to
-    // buffer more data.
     MEDIA_INFO_BUFFERING_START = 701,
-    // MediaPlayer is resuming playback after filling buffers.
     MEDIA_INFO_BUFFERING_END = 702,
-    // Bandwidth in recent past
     MEDIA_INFO_NETWORK_BANDWIDTH = 703,
     
-    // 8xx
-    // Bad interleaving means that a media has been improperly interleaved or not
-    // interleaved at all, e.g has all the video samples first then all the audio
-    // ones. Video is playing but a lot of disk seek may be happening.
     MEDIA_INFO_BAD_INTERLEAVING = 800,
-    // The media is not seekable (e.g live stream).
     MEDIA_INFO_NOT_SEEKABLE = 801,
-    // New media metadata is available.
     MEDIA_INFO_METADATA_UPDATE = 802,
 };
 
-
+//播放器错误分类
+enum media_error_type {
+    MEDIA_ERROR_UNKNOWN = 1,
+    MEDIA_ERROR_SERVER_DIED = 100,
+    MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK = 200,
+};
 
 enum media_player_states {
     MEDIA_PLAYER_STATE_ERROR        = 0,
@@ -97,8 +79,6 @@ enum media_player_states {
     MEDIA_PLAYER_PLAYBACK_COMPLETE  = 1 << 7
 };
 
-// ----------------------------------------------------------------------------
-// ref-counted object for callbacks
 class MediaPlayerListener
 {
 public:
@@ -109,41 +89,52 @@ public:
 class IPlayer
 {
 public:
-	virtual void notify(int msg, int ext1, int ext2) = 0;
-	virtual void disconnect() = 0;
-	virtual status_t suspend() = 0;
-	virtual status_t resume() = 0;
+	//播放器初始化接口
 	virtual status_t setDataSource(const char *url) = 0;
-	virtual status_t setDataSource(int fd, int64_t offset, int64_t length) = 0;
 	virtual status_t setVideoSurface(void* surface) = 0;
 	virtual status_t prepare() = 0;
 	virtual status_t prepareAsync() = 0;
+
+	//播放器控制接口
 	virtual status_t start() = 0;
 	virtual status_t stop() = 0;
 	virtual status_t pause() = 0;
-	virtual bool isPlaying() = 0;
 	virtual status_t seekTo(int msec) = 0;
-	virtual	status_t getVideoWidth(int *w) = 0;
+
+	//播放器状态回调接口
+	virtual status_t setListener(MediaPlayerListener* listener) = 0;
+
+	//播放器状态获取接口
+	virtual status_t getVideoWidth(int *w) = 0;
 	virtual status_t getVideoHeight(int *h) = 0;
 	virtual status_t getCurrentPosition(int *msec) = 0;
 	virtual status_t getDuration(int *msec) = 0;
-	virtual status_t reset() = 0;
-	virtual status_t setAudioStreamType(int type) = 0;
-	virtual status_t setLooping(int loop) = 0;
-	virtual bool isLooping() = 0;
-	virtual status_t setVolume(float leftVolume, float rightVolume) = 0;
-	virtual status_t setListener(MediaPlayerListener* listener) = 0;
+	virtual bool getMediaInfo(const char* url, MediaInfo* info){return false;} 
+	virtual bool getMediaDetailInfo(const char* url, MediaInfo* info){return false;}
+	virtual bool getThumbnail(const char* url, MediaInfo* info){return false;}
 	virtual int flags() = 0;
+	
+	virtual ~IPlayer() {}
+
+	//下面是不常用的接口
+	
 #ifdef OS_ANDROID
 	virtual status_t startCompatibilityTest() = 0;
 	virtual void stopCompatibilityTest() = 0;
 #endif
 	virtual status_t startP2PEngine() = 0;
 	virtual void stopP2PEngine() = 0;
-	virtual ~IPlayer() {}
-	virtual bool getMediaInfo(const char* url, MediaInfo* info){return false;} 
-	virtual bool getMediaDetailInfo(const char* url, MediaInfo* info){return false;}
-	virtual bool getThumbnail(const char* url, MediaInfo* info){return false;}
+	virtual status_t reset() = 0;
+	virtual status_t setAudioStreamType(int type) = 0;
+	virtual status_t setLooping(int loop) = 0;
+	virtual bool isLooping() = 0;
+	virtual status_t setVolume(float leftVolume, float rightVolume) = 0;
+	virtual bool isPlaying() = 0;
+	virtual status_t setDataSource(int fd, int64_t offset, int64_t length) = 0;
+	virtual void notify(int msg, int ext1, int ext2) = 0;
+	virtual void disconnect() = 0;
+	virtual status_t suspend() = 0;
+	virtual status_t resume() = 0;
 };
 
 #endif
